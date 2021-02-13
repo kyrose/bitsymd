d94830d0e9ae3b0a974a46c66b7b04573f26e011
kyrose <dev@kyanne.email>
2021-02-12 21:31:32 -0800

    Complete functions to output program banner

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,81 @@
-fn parse_markdown_file() {}
-
+// MAIN OBJECTIVES //
 // Print a banner with package information
 // Output:
-//      [CARGO_PKG_NAME] (v[CARGO_PKG_VERSION]), [CARGO_PKG_DESCRIPTION]
-//      Written by [CARGO_PKG_AUTHORS]
-//      Usage: bitsymd <somefile.md>
-//
+//  //  [CARGO_PKG_NAME] (v[CARGO_PKG_VERSION]), [CARGO_PKG_DESCRIPTION]
+//  //  Written by [CARGO_PKG_AUTHORS]
+//  //  Homepage: [CARGO_PKG_HOMEPAGE]
+//  //  Usage: bitsymd <somefile.md>
+
+fn get_title() -> String {
+    // Create local String var to hold data we want for our output
+    // Var must be mutable since we'll append other strings to it
+    // The strings to append (ref output at top):
+    //  1.  " (v"
+    //  2.  env!("CARGO_PKG_VERSION")
+    //  3.  "), "
+    //  4.  env!("CARGO_PKG_DESCRIPTION")
+    //
+    let mut the_title = String::from(env!("CARGO_PKG_NAME"));
+
+    // To append a `&str` use .push_str()
+    the_title.push_str(" (v");
+    the_title.push_str(env!("CARGO_PKG_VERSION"));
+    the_title.push_str("), ");
+    the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+
+    // Need a return statement for the get_title function to actually return the string
+    // we just built!
+    return the_title;
+}
+
+// Print first line of banner (title, version, description)
+fn print_short_banner() {
+    // println!() only accepts string literals, so we must use substitution ("{}")
+    println!("{}", get_title());
+}
+
+// Print full banner (short_banner + author + usage example)
+fn print_long_banner() {
+    // Create a string variable for each line of the banner. First line covered already.
+    //  1. "Written by: " + env!("CARGO_PKG_AUTHORS")
+    //  2. "Homepage: " + env!("CARGO_PKG_HOMEPAGE")
+    //  3. "Usage: bitsymd <somefile>.md"
+    //
+    // 1.
+    let mut the_author = String::from("Written by: ");
+    the_author.push_str(env!("CARGO_PKG_AUTHORS"));
+
+    // 2.
+    let mut the_homepage = String::from("Homepage: ");
+    the_homepage.push_str(env!("CARGO_PKG_HOMEPAGE"));
+
+    // 3.
+    //let the_usage = "Usage: bitsymd <filename>.md"
+
+    // Print out
+    print_short_banner();
+    println!("{}", the_author);
+    println!("{}", the_homepage);
+    println!("Usage: bitsymd <filename>.md");
+}
+
+// A far more elegant solution
+// fn print_long_banner() {
+//     print_short_banner();
+//     println!("Written by: {}\nHomepage: {}\n Usage: bitsymd <filename>.md\n",
+//         env!("CARGO_PKG_AUTHORS"),
+//         env!("CARGO_PKG_HOMEPAGE")
+//     );
+// }
+
 fn usage() {}
 
+// Called when markdown file is passed via cli
+fn parse_markdown_file() {
+
+}
+
 fn main() {
+    print_long_banner();
     usage();
 }
